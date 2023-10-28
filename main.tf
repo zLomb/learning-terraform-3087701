06d48d4f069f6779f1660864e2927039ae63e852
zLomb <79952617+zLomb@users.noreply.github.com>
2023-10-28 16:48:46 -0300

    Refactor to use variables

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ data "aws_ami" "app_ami" {
 
   filter {
     name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+    values = [var.ami_filter.name]
   }
 
   filter {
@@ -11,7 +11,7 @@ data "aws_ami" "app_ami" {
     values = ["hvm"]
   }
 
-  owners = ["979382823631"] # Bitnami
+  owners = [var.ami_filter.owner]
 }
 
 data "aws_vpc" "default" {
@@ -21,15 +21,15 @@ data "aws_vpc" "default" {
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "dev"
-  cidr = "10.0.0.0/16"
+  name = var.environment.name
+  cidr = "${var.environment.network_prefix}.0.0/16"
 
   azs             = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  public_subnets  = ["${var.environment.network_prefix}.101.0/24", "${var.environment.network_prefix}.102.0/24", "${var.environment.network_prefix}.103.0/24"]
 
   tags = {
     Terraform = "true"
-    Environment = "dev"
+    Environment = var.environment.name
   }
 }
 
@@ -39,8 +39,8 @@ module "autoscaling" {
   # insert the 1 required variable here
 
   name = "blog"
-  min_size = 1
-  max_size = 2
+  min_size = var.min_size
+  max_size = var.max_size
 
   vpc_zone_identifier = module.blog_vpc.public_subnets
   target_group_arns = module.blog_alb.target_group_arns
@@ -54,7 +54,7 @@ module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 6.0"
 
-  name = "blog-alb"
+  name = "${var.environment.name}-blog-alb"
 
   load_balancer_type = "application"
 
@@ -64,7 +64,7 @@ module "blog_alb" {
 
   target_groups = [
     {
-      name_prefix      = "blog-"
+      name_prefix      = "${var.enviornment.name}-"
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
@@ -80,7 +80,7 @@ module "blog_alb" {
   ]
 
   tags = {
-    Environment = "dev"
+    Environment = var.environment.name
   }
 }
 
@@ -89,7 +89,7 @@ module "blog_sg" {
   version = "4.13.0"
 
 
-  name = "blog"
+  name = "${var.environment.name}-blog"
   vpc_id = module.blog_vpc.vpc_id
 
   ingress_rules = ["http-80-tcp", "https-443-tcp"]
@@ -100,7 +100,7 @@ module "blog_sg" {
 }
 
 resource "aws_security_group" "blog" {
-  name = "blog"
+  name = "${var.environment.name}-blog"
   description = "Allow http and https in. Allow everything out"
 
   vpc_id = data.aws_vpc.default.id

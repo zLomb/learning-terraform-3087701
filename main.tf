e13c85d8786d867c166ae207a90ad074eeb9237d
zLomb <79952617+zLomb@users.noreply.github.com>
2023-10-28 15:40:05 -0300

    load balancer security groups fix

--- main.tf
+++ main.tf
@@ -52,7 +52,7 @@ module "alb" {
   name    = "blog-alb"
   vpc_id  = module.blog_vpc.vpc_id
   subnets = module.blog_vpc.public_subnets
-  security_groups = module.blog_sg.security_group_id
+  security_groups = [module.blog_sg.security_group_id]
 
   # Security Group
   security_group_ingress_rules = {

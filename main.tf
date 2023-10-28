7182620a1dbd6b1927ab608df54e4abdb844212f
zLomb <79952617+zLomb@users.noreply.github.com>
2023-10-28 16:30:16 -0300

    blog_alb deleting https and adding target_id

--- main.tf
+++ main.tf
@@ -52,7 +52,7 @@ module "autoscaling" {
 module "blog_alb" {
   source = "terraform-aws-modules/alb/aws"
 
-  name    = "blog-alb"
+  name    = "blog_alb"
   vpc_id  = module.blog_vpc.vpc_id
   subnets = module.blog_vpc.public_subnets
   security_groups = [module.blog_sg.security_group_id]
@@ -86,23 +86,10 @@ module "blog_alb" {
   }
 
   listeners = {
-    ex-http-https-redirect = {
+    http_tcp_listeners = {
       port     = 80
       protocol = "HTTP"
-      redirect = {
-        port        = "443"
-        protocol    = "HTTPS"
-        status_code = "HTTP_301"
-      }
-    }
-    ex-https = {
-      port            = 443
-      protocol        = "HTTPS"
-      certificate_arn = "arn:aws:iam::123456789012:server-certificate/test_cert-123456789012"
-
-      forward = {
-        target_group_key = "ex-instance"
-      }
+      target_group_index = 0
     }
   }
 
@@ -112,12 +99,17 @@ module "blog_alb" {
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
+      targets = {
+        my_target = {
+          target_id = aws_instance.blog.id
+          port = 80
+        }
+      }
     }
   }
 
   tags = {
-    Environment = "Development"
-    Project     = "Example"
+    Environment = "dev"
   }
 }
 

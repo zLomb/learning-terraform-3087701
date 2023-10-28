06298964af52ee2ce44bf0727c834603af676934
zLomb <79952617+zLomb@users.noreply.github.com>
2023-10-28 16:23:45 -0300

    arns

--- main.tf
+++ main.tf
@@ -43,7 +43,6 @@ module "autoscaling" {
   max_size = 2
 
   vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_group_arns = module.blog_alb.target_group_arns
   security_groups = [module.blog_sg.security_group_id]
 
   image_id           = data.aws_ami.app_ami.id

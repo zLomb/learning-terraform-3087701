4c6fc458a12b824ee4f398883767002e6a83a876
zLomb <79952617+zLomb@users.noreply.github.com>
2023-10-28 16:33:22 -0300

    .

--- main.tf
+++ main.tf
@@ -43,6 +43,7 @@ module "autoscaling" {
   max_size = 2
 
   vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns = module.blog_alb.target_group_arns
   security_groups = [module.blog_sg.security_group_id]
 
   image_id           = data.aws_ami.app_ami.id
@@ -99,12 +100,6 @@ module "blog_alb" {
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
-      targets = {
-        my_target = {
-          target_id = aws_instance.blog.id
-          port = 80
-        }
-      }
     }
   }
 
